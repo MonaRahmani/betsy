1f57615ee322711484734113d844273b23b390e8
Anya Tokar <61166946+anyatokar@users.noreply.github.com>
2020-11-17 17:52:50 -0800

    product starter data set

--- db/generate_starter_data.rb
+++ db/generate_starter_data.rb
@@ -3,16 +3,31 @@ require "date"
 require "csv"
 
 # to run: ruby db/generate_starter_data.rb
-# review works_seeds.csv file
+# review products_seeds.csv file
 # recreate the db with: rails db:reset
 # note: doesn't currently check for if names are unique against each other
 
-CSV.open("db/users_seeds.csv", "w", :write_headers => true,
-         :headers => ["username", "email"]) do |csv|
-  25.times do
-    username = Faker::Name.name
-    email = username.split(" ").join.downcase + "@dream-team.com"
 
-    csv << [username, email]
+CSV.open("db/products_seeds.csv", "w", :write_headers => true,
+         :headers => ["name", "description", "price", "photo_url", "stock", "retired"]) do |csv|
+  20.times do
+    name = ""
+    description = Faker::Quote.yoda
+    price = 0
+    photo_url = "https://i.pinimg.com/564x/a9/f3/b1/a9f3b1e3aef96d52fca5e01d3fb22434.jpg"
+    stock = 0
+    retired = true
+
+    csv << [name, description, price, photo_url, stock, retired]
   end
 end
+#
+# CSV.open("db/users_seeds.csv", "w", :write_headers => true,
+#          :headers => ["username", "email"]) do |csv|
+#   25.times do
+#     username = Faker::Name.name
+#     email = username.split(" ").join.downcase + "@dream-team.com"
+#
+#     csv << [username, email]
+#   end
+# end
