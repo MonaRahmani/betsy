266ec12ca984971e06376a2482e437196cf3b587
MonaRahmani <monarahmanimehr@gmail.com>
2020-11-18 08:09:24 -0800

    added category actions

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,24 @@
 class CategoriesController < ApplicationController
+
+  def new
+    @category = category.new
+  end
+
+  def create
+    @category = category.create(category_params)
+    if @category.id?
+      flash[:success] = "Successfully added #{@category.category_name} category"
+      redirect_to user_path(session[:user_id])
+    else
+      flash[:result_text] = "Could not create category"
+    end
+
+    def category_params
+      params.require(:category).permit(:category_name)
+    end
+
+  end
 end
+
+
+
