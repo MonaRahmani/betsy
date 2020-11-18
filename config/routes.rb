878409a0d4b90003b54563b004e0201d8389ab02
KayKay-git <kayalew@yahoo.com>
2020-11-18 01:14:45 -0500

    Added user route for logging out

--- config/routes.rb
+++ config/routes.rb
@@ -22,4 +22,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/auth/github", as: "github_login"
   get "/auth/:provider/callback", to: "users#create"
+  delete "/logout", to: "users#destroy", as: "logout"
 end
