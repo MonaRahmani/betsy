72d14817dc0a416ba89e65ab7541b5bfc63a472e
KayKay-git <kayalew@yahoo.com>
2020-11-18 01:13:00 -0500

    Updated User model to include columns for uid and provider

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_18_033754) do
+ActiveRecord::Schema.define(version: 2020_11_18_061032) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,8 @@ ActiveRecord::Schema.define(version: 2020_11_18_033754) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "uid"
+    t.string "provider"
   end
 
 end
