4797f02af48e51e05df78eb02c2b2c86f954fe4b
KayKay-git <kayalew@yahoo.com>
2020-11-19 22:06:09 -0500

    order item subtotal method created

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,10 @@
 class OrderItem < ApplicationRecord
   belongs_to :product
   belongs_to :order
+  validates :quantity, presence: true, numericality: { only_integer: true }
+
+
+  def order_subtotal
+    self.quantity * self.product.price
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,37 +1,46 @@
-has_many :products
-validates :username, presence: true, uniqueness: true
-validates :email, presence: true, uniqueness: true
-validates :uid, presence: true, uniqueness: true
+class User < ApplicationRecord
+  has_many :products
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :uid, presence: true, uniqueness: true
 
 
-# build a user from github hash info
-def self.build_from_github(auth_hash)
-  user = User.new
-  user.uid = auth_hash[:uid]
-  user.provider = "github"
-  user.username = auth_hash["info"]["nickname"]
-  user.email = auth_hash["info"]["email"]
+  # build a user from github hash info
+  def self.build_from_github(auth_hash)
+    user = User.new
+    user.uid = auth_hash[:uid]
+    user.provider = "github"
+    user.username = auth_hash["info"]["nickname"]
+    user.email = auth_hash["info"]["email"]
 
-  # Note that the user has not been saved. We'll choose to do the saving outside of this method.(in create)
-  return user
-end
+    # Note that the user has not been saved. We'll choose to do the saving outside of this method.(in create)
+    return user
+  end
 
-# Total Revenue
-def total_rev
+  # Total Revenue
+  def total_rev
+    sum = 0
+    # iterate through user products to find particular order item price
+    self.products.each do |product|
+      product.order_items.each do |item|
 
-end
+      end
+    end
+  end
 
-# Total Revenue by status
-def total_rev_status
+  # Total Revenue by status
+  def total_rev_status
 
-end
+  end
 
-# Total number of orders by status
-def total_num_orders_by_status
+  # Total number of orders by status
+  def total_num_orders_by_status
 
-end
+  end
 
-# Filter orders displayed by status
-def orders_filtered_by_status
+  # Filter orders displayed by status
+  def orders_filtered_by_status
+
+  end
 
 end
