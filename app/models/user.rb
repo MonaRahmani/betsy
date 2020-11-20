5ef6820646cb7efe8925630087cca4228ff9b0b7
KayKay-git <kayalew@yahoo.com>
2020-11-19 19:19:18 -0500

    user model methods created, updated validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,37 @@
-class User < ApplicationRecord
-  has_many :products
-  validates :username, presence: true
-  validates :email, presence: true
+has_many :products
+validates :username, presence: true, uniqueness: true
+validates :email, presence: true, uniqueness: true
+validates :uid, presence: true, uniqueness: true
 
-  def self.build_from_github(auth_hash)
-    user = User.new
-    user.uid = auth_hash[:uid]
-    user.provider = "github"
-    user.username = auth_hash["info"]["nickname"]
-    user.email = auth_hash["info"]["email"]
 
-    # Note that the user has not been saved.
-    # We'll choose to do the saving outside of this method
-    return user
-  end
+# build a user from github hash info
+def self.build_from_github(auth_hash)
+  user = User.new
+  user.uid = auth_hash[:uid]
+  user.provider = "github"
+  user.username = auth_hash["info"]["nickname"]
+  user.email = auth_hash["info"]["email"]
+
+  # Note that the user has not been saved. We'll choose to do the saving outside of this method.(in create)
+  return user
+end
+
+# Total Revenue
+def total_rev
+
+end
+
+# Total Revenue by status
+def total_rev_status
+
+end
+
+# Total number of orders by status
+def total_num_orders_by_status
+
+end
+
+# Filter orders displayed by status
+def orders_filtered_by_status
+
 end
