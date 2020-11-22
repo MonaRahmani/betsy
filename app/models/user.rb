f17eed659c95dd77e1ab85fe6413e96c8209cecc
Anya Tokar <61166946+anyatokar@users.noreply.github.com>
2020-11-22 11:28:26 -0800

    db seed working

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :order_items, through: :products
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
-  # validates :uid, presence: true, uniqueness: true
+  validates :uid, presence: true, uniqueness: true
 
 
   # build a user from github hash info
