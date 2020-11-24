c1f85a516085e70bcaa1d563f60e501ded308953
MonaRahmani <monarahmanimehr@gmail.com>
2020-11-23 20:48:43 -0800

    category test passing

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -26,14 +26,14 @@ describe CategoriesController do
       expect(Category.last.category_name).must_equal category_hash[:category][:category_name]
     end
 
-    # it "will not create a category with invalid params" do
-    #   category_hash[:category][:category_name] = nil
-    #
-    #   expect {
-    #     post categories_path, params: category_hash
-    #   }.must_differ "Category.count", 0
-    #
-    #   must_respond_with :bad_request
-    # end
+    it "will not create a category with invalid params" do
+      category_hash[:category][:category_name] = nil
+
+      expect {
+        post categories_path, params: category_hash
+      }.wont_change "Category.count", 0
+
+      must_respond_with :bad_request
+    end
   end
 end
