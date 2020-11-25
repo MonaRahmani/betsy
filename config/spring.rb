702eaa2c1948736f8cbc5989673eb2f5ff54c7bc
Anya Tokar <61166946+anyatokar@users.noreply.github.com>
2020-11-24 18:03:25 -0800

    added simplecov

--- config/spring.rb
+++ config/spring.rb
@@ -1,6 +1,14 @@
+# if ENV['RAILS_ENV'] == 'test'
+#   require 'simplecov'
+#   SimpleCov.start 'rails'
+#   puts "required simplecov"
+# end
+#
 Spring.watch(
   ".ruby-version",
   ".rbenv-vars",
   "tmp/restart.txt",
   "tmp/caching-dev.txt"
 )
+
+
